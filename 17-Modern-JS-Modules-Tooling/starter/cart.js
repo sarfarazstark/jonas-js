5541dd6dfcd8e09ab004e741661c138651dee638
sarfarazstark <sarfarazstark@gmail.com>
2024-07-24 23:34:18 +0530

    Lecture 257 is done

--- 17-Modern-JS-Modules-Tooling/starter/cart.js
+++ 17-Modern-JS-Modules-Tooling/starter/cart.js
@@ -1,5 +1,11 @@
 // Export
 console.log('Export');
+
+// blocking code
+// console.log('Start fetching');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching');
+
 export const cart = [];
 
 export default function (item, quantity) {
--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // import { addItem as add } from './cart.js';
 // console.log('Import');
 // add('apple', 13);
@@ -7,3 +8,27 @@ add('apple', 13);
 add('bread', 8);
 add('banana', 18);
 console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  // console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// not clean
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
